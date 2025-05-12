ed014e3a90d8fe5d7618a2674fb13eae87d60213
Allexzaa <alex.zare99@gmail.com>
2025-05-12 12:55:32 -0700

    Update scripts.js

--- sidebar1/scripts.js
+++ sidebar1/scripts.js
@@ -1,8 +1,7 @@
 const sidebar = document.getElementById('floating-sidebar');
-let lastScroll = window.scrollY;
 
 function followScroll() {
-  const targetTop = window.scrollY + 100;
+  const targetTop = window.scrollY + 50;
   const currentTop = parseFloat(getComputedStyle(sidebar).top);
   const newTop = currentTop + (targetTop - currentTop) * 0.1;
   sidebar.style.top = `${newTop}px`;
